a5598f1e4358cf9258ebb6de486f4ffc1d090b63
masayuki_oguni <masayuki_oguni@dwango.co.jp>
2014-08-28 19:37:19 +0900

    update

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-type WinFormat struct {
+type WinPacket struct {
 	sequence    uint8
 	subSequence uint8
 	A0          byte
@@ -28,42 +28,35 @@ type WinFormat struct {
 	Sampling    []int32
 }
 
-func (winFormat *WinFormat) GetSequence() int {
-	return int(winFormat.sequence)
+type WinFormat struct {
+	Sequence    uint8
+	SubSequence uint8
+	A0          byte
+	Length      uint16
+	Datetime    int64
+	Channel     uint16
+	Rate        uint16
+	Size        uint16
+	FirstSample int32
+	Sampling    []int32
 }
 
-func (winFormat *WinFormat) GetSubSequence() int {
-	return int(winFormat.subSequence)
+func (winPacket *WinPacket) GetSize() uint16 {
+	return uint16(winPacket.size >> 4)
 }
 
-func (winFormat *WinFormat) GetLength() int {
-	return int(winFormat.length)
+func (winPacket *WinPacket) GetRate() uint16 {
+	return uint16((winPacket.size&0x0f)<<8 | winPacket.rate&0xff)
 }
 
-func (winFormat *WinFormat) GetChannel() int {
-	return int(winFormat.channel)
-}
-
-func (winFormat *WinFormat) GetSamplingSize() int {
-	return int(winFormat.size >> 4)
-}
-
-func (winFormat *WinFormat) GetSamplingRate() int {
-	return int((winFormat.size&0x0f)<<8 | winFormat.rate&0xff)
-}
-
-func (winFormat *WinFormat) GetFirstSample() int {
-	return int(winFormat.firstSample)
-}
-
-func (winFormat *WinFormat) GetUnixDateTime() int64 {
+func (winPacket *WinPacket) GetUnixDateTime() int64 {
 	datetime := fmt.Sprintf("20%02d-%02d-%02dT%02d:%02d:%02d+09:00",
-		bcd.BcdToInt(int(winFormat.year)),
-		bcd.BcdToInt(int(winFormat.month)),
-		bcd.BcdToInt(int(winFormat.day)),
-		bcd.BcdToInt(int(winFormat.hour)),
-		bcd.BcdToInt(int(winFormat.minute)),
-		bcd.BcdToInt(int(winFormat.second)))
+		bcd.BcdToInt(int(winPacket.year)),
+		bcd.BcdToInt(int(winPacket.month)),
+		bcd.BcdToInt(int(winPacket.day)),
+		bcd.BcdToInt(int(winPacket.hour)),
+		bcd.BcdToInt(int(winPacket.minute)),
+		bcd.BcdToInt(int(winPacket.second)))
 	t, err := time.Parse(
 		time.RFC3339, datetime)
 	if err != nil {
@@ -72,28 +65,38 @@ func (winFormat *WinFormat) GetUnixDateTime() int64 {
 	return t.Unix()
 }
 
+func NewWinFormat(buffer []byte) *WinFormat {
+	return Parse(buffer)
+}
+
 func Parse(buffer []byte) *WinFormat {
-	winformat := WinFormat{}
+	winPacket := WinPacket{}
 	buf := bytes.NewBuffer(buffer)
-	binary.Read(buf, binary.BigEndian, &winformat.sequence)
-	binary.Read(buf, binary.BigEndian, &winformat.subSequence)
-	binary.Read(buf, binary.BigEndian, &winformat.A0)
-	binary.Read(buf, binary.BigEndian, &winformat.length)
-	binary.Read(buf, binary.BigEndian, &winformat.year)
-	binary.Read(buf, binary.BigEndian, &winformat.month)
-	binary.Read(buf, binary.BigEndian, &winformat.day)
-	binary.Read(buf, binary.BigEndian, &winformat.hour)
-	binary.Read(buf, binary.BigEndian, &winformat.minute)
-	binary.Read(buf, binary.BigEndian, &winformat.second)
-	binary.Read(buf, binary.BigEndian, &winformat.channel)
-	binary.Read(buf, binary.BigEndian, &winformat.size)
-	binary.Read(buf, binary.BigEndian, &winformat.rate)
-	binary.Read(buf, binary.BigEndian, &winformat.firstSample)
+	binary.Read(buf, binary.BigEndian, &winPacket.sequence)
+	binary.Read(buf, binary.BigEndian, &winPacket.subSequence)
+	binary.Read(buf, binary.BigEndian, &winPacket.A0)
+	binary.Read(buf, binary.BigEndian, &winPacket.length)
+	binary.Read(buf, binary.BigEndian, &winPacket.year)
+	binary.Read(buf, binary.BigEndian, &winPacket.month)
+	binary.Read(buf, binary.BigEndian, &winPacket.day)
+	binary.Read(buf, binary.BigEndian, &winPacket.hour)
+	binary.Read(buf, binary.BigEndian, &winPacket.minute)
+	binary.Read(buf, binary.BigEndian, &winPacket.second)
+	binary.Read(buf, binary.BigEndian, &winPacket.channel)
+	binary.Read(buf, binary.BigEndian, &winPacket.size)
+	binary.Read(buf, binary.BigEndian, &winPacket.rate)
+	binary.Read(buf, binary.BigEndian, &winPacket.firstSample)
 
-	size := winformat.size >> 4
-	rate := (winformat.size&0x0f)<<8 | winformat.rate&0xff
-
-	winformat.Sampling = make([]int32, rate-1)
+	winFormat := WinFormat{
+		Sequence:    winPacket.sequence,
+		SubSequence: winPacket.subSequence,
+		Length:      winPacket.length,
+		Datetime:    winPacket.GetUnixDateTime(),
+		Channel:     winPacket.channel,
+		Rate:        winPacket.GetRate(),
+		Size:        winPacket.GetSize(),
+		FirstSample: winPacket.firstSample,
+	}
 
 	/*
 		fmt.Printf("%d %d %d %d %d %d\n", int(winformat.year),
@@ -120,41 +123,67 @@ func Parse(buffer []byte) *WinFormat {
 			winformat.FirstSample,
 		)
 	*/
-	if size == 0 {
-		rate = rate / 2
 
+	if winFormat.Size == 0 {
+		winFormat.Rate = winFormat.Rate / 2
 	}
 
-	for i := 0; i < int(rate)-1; i++ {
-		if size == 4 {
+	winFormat.Sampling = make([]int32, winFormat.Rate-1)
+
+	for i := 0; i < int(winFormat.Rate)-1; i++ {
+
+		switch winFormat.Size {
+		case 4:
 			var value int32
 			binary.Read(buf, binary.BigEndian, &value)
-			winformat.Sampling[i] = value
-		}
-		if size == 3 {
+			winFormat.Sampling[i] = value
+		case 3:
 			value := make([]byte, 3)
 			binary.Read(buf, binary.BigEndian, &value)
 			num, _ := strconv.ParseInt(fmt.Sprintf("%x%x%x", value[0], value[1], value[2]), 16, 32)
-			winformat.Sampling[i] = int32(num)
-		}
-		if size == 2 {
+			winFormat.Sampling[i] = int32(num)
+		case 2:
 			var value int16
 			binary.Read(buf, binary.BigEndian, &value)
-			winformat.Sampling[i] = int32(value)
+			winFormat.Sampling[i] = int32(value)
 
-		}
-		if size == 1 {
+		case 1, 0:
 			var value int8
 			binary.Read(buf, binary.BigEndian, &value)
-			winformat.Sampling[i] = int32(value)
-
-		}
-		if size == 0 {
-			var value int8
-			binary.Read(buf, binary.BigEndian, &value)
-			winformat.Sampling[i] = int32(value)
+			winFormat.Sampling[i] = int32(value)
 		}
 	}
+	/*
+			if winFormat.Size == 4 {
+				var value int32
+				binary.Read(buf, binary.BigEndian, &value)
+				winFormat.Sampling[i] = value
+			}
+			if winFormat.Size == 3 {
+				value := make([]byte, 3)
+				binary.Read(buf, binary.BigEndian, &value)
+				num, _ := strconv.ParseInt(fmt.Sprintf("%x%x%x", value[0], value[1], value[2]), 16, 32)
+				winFormat.Sampling[i] = int32(num)
+			}
+			if winFormat.Size == 2 {
+				var value int16
+				binary.Read(buf, binary.BigEndian, &value)
+				winFormat.Sampling[i] = int32(value)
 
-	return &winformat
+			}
+			if winFormat.Size == 1 {
+				var value int8
+				binary.Read(buf, binary.BigEndian, &value)
+				winFormat.Sampling[i] = int32(value)
+
+			}
+			if winFormat.Size == 0 {
+				var value int8
+				binary.Read(buf, binary.BigEndian, &value)
+				winFormat.Sampling[i] = int32(value)
+			}
+		}
+	*/
+
+	return &winFormat
 }
--- main_test.go
+++ main_test.go
@@ -1,15 +1,14 @@
 package winformat
 
 import (
+	//"fmt"
 	. "github.com/r7kamura/gospel"
 	"os"
 	"testing"
 )
 
 func ReadBinaryFile(filename string) []byte {
-	// ファイルを開く
 	file, _ := os.Open(filename)
-	// ファイルから1バイト読み出し
 	b := make([]byte, 1400)
 	file.Read(b)
 	return b
@@ -18,83 +17,98 @@ func ReadBinaryFile(filename string) []byte {
 func TestDescribe(t *testing.T) {
 	Describe(t, "Parse WinFormat Data", func() {
 		Context("when SamplingSize is 4", func() {
-			winFormat := Parse(ReadBinaryFile("./testdata/4.bin"))
+			winFormat := NewWinFormat(ReadBinaryFile("./testdata/4.bin"))
 
 			It("Sequence is 0 ", func() {
-				Expect(winFormat.GetSequence()).To(Equal, 0)
+				Expect(winFormat.Sequence).To(Equal, uint8(0))
 			})
 
 			It("SubSequence is 0", func() {
-				Expect(winFormat.GetSubSequence()).To(Equal, 0)
+				Expect(winFormat.SubSequence).To(Equal, uint8(0))
 			})
 
 			It("channel is 0", func() {
-				Expect(winFormat.GetChannel()).To(Equal, 0)
+				Expect(winFormat.Channel).To(Equal, uint16(0))
 			})
 
 			It("SamplingRate is 200", func() {
-				Expect(winFormat.GetSamplingRate()).To(Equal, 200)
+				Expect(winFormat.Rate).To(Equal, uint16(200))
 			})
 
-			It("SamplingSize is 4", func() {
-				Expect(winFormat.GetSamplingSize()).To(Equal, 4)
+			It("Size is 4", func() {
+				Expect(winFormat.Size).To(Equal, uint16(4))
 			})
 
 			It("datetime is 1409050412", func() {
-				Expect(winFormat.GetUnixDateTime()).To(Equal, int64(1409050412))
+				Expect(winFormat.Datetime).To(Equal, int64(1409050412))
 			})
 
 			It("firstsample is ", func() {
-				Expect(winFormat.GetFirstSample()).To(Equal, 447712320)
+				Expect(winFormat.FirstSample).To(Equal, int32(447712320))
 			})
 
+			It("Sampleing Length is 199 ", func() {
+				Expect(len(winFormat.Sampling)).To(Equal, 199)
+			})
 		})
 
 		Context("when SamplingSize is 3", func() {
 			winFormat := Parse(ReadBinaryFile("./testdata/3.bin"))
-			It("Sequence is 2 ", func() {
-				Expect(winFormat.GetSequence()).To(Equal, 2)
+			It("Sequence is 0 ", func() {
+				Expect(winFormat.Sequence).To(Equal, uint8(2))
 			})
 
-			It("SubSequence is 2", func() {
-				Expect(winFormat.GetSubSequence()).To(Equal, 2)
+			It("SubSequence is 0", func() {
+				Expect(winFormat.SubSequence).To(Equal, uint8(2))
 			})
 
-			It("channel is 1", func() {
-				Expect(winFormat.GetChannel()).To(Equal, 1)
+			It("channel is 0", func() {
+				Expect(winFormat.Channel).To(Equal, uint16(1))
 			})
 
-			It("SamplingRate is 100", func() {
-				Expect(winFormat.GetSamplingRate()).To(Equal, 100)
+			It("SamplingRate is 200", func() {
+				Expect(winFormat.Rate).To(Equal, uint16(100))
 			})
 
-			It("SamplingSize is 3", func() {
-				Expect(winFormat.GetSamplingSize()).To(Equal, 3)
+			It("Size is 3", func() {
+				Expect(winFormat.Size).To(Equal, uint16(3))
 			})
 
-			It("datetime is 1409111848", func() {
-				Expect(winFormat.GetUnixDateTime()).To(Equal, int64(1409111848))
+			It("datetime is 1409050412", func() {
+				Expect(winFormat.Datetime).To(Equal, int64(1409111848))
 			})
+
+			It("Sampleing Length is 99 ", func() {
+				Expect(len(winFormat.Sampling)).To(Equal, 99)
+			})
+
 		})
 
 		Context("when SamplingSize is 2", func() {
 			winFormat := Parse(ReadBinaryFile("./testdata/2.bin"))
-			It("SamplingSize is 2", func() {
-				Expect(winFormat.GetSamplingSize()).To(Equal, 2)
+
+			It("Size is 2", func() {
+				Expect(winFormat.Size).To(Equal, uint16(2))
 			})
 		})
 
 		Context("when SamplingSize is 1", func() {
 			winFormat := Parse(ReadBinaryFile("./testdata/1.bin"))
-			It("SamplingSize is 1", func() {
-				Expect(winFormat.GetSamplingSize()).To(Equal, 1)
+
+			It("Size is 1", func() {
+				Expect(winFormat.Size).To(Equal, uint16(1))
 			})
 		})
 
 		Context("when SamplingSize is 0", func() {
 			winFormat := Parse(ReadBinaryFile("./testdata/0.bin"))
-			It("SamplingSize is 0", func() {
-				Expect(winFormat.GetSamplingSize()).To(Equal, 0)
+
+			It("Size is 0", func() {
+				Expect(winFormat.Size).To(Equal, uint16(0))
+			})
+
+			It("Sampleing Length is 99 ", func() {
+				Expect(len(winFormat.Sampling)).To(Equal, 99)
 			})
 		})
 
